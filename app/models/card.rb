91d54eedf1df43d6a8e6dd6bb841a0b3af964339
Sabiha Ozturk <sabihay@hotmail.com>
2015-07-15 21:28:13 -0400

    Added validations for models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,23 @@
 class Card < ActiveRecord::Base
+  validates_format_of :number, with: /\A[345]\d{11,15}\Z/
+  validates :exp_month, inclusion: {in: (1..12)}
+  validates :exp_year, inclusion: {in:(2015..2115)}
+
+  before_save :set_card_type
+
+  def set_card_type
+    first_num = self.number[0]
+    self.type = case first_num
+    when '3'
+      'American Express'
+    when '4'
+      'Visa'
+    when '5'
+      'Mastercard'
+    end
+  end
+
+
+  has_many :users, through: :user_cards
+  has_many :user_cards
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates_presence_of :fname, :lname, :email, :password 
+  validates_uniqueness_of :email
+  validates_format_of :phone, with: /\d{3}-\d{3}-\d{4}/
+  validates_numericality_of :balance {greater_than_or_equal_to: 0}
+  
+
+  has_many :cards, through: :user_cards
+  has_many :user_cards
 end
