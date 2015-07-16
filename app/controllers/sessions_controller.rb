f085bccf244d8ec686eb0f14bfba539cd7937758
Sabiha Ozturk <sabihay@hotmail.com>
2015-07-16 00:16:02 -0400

    Added Signup, Login and Logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    session[:user_id] = nil
+  end
+  
+  def create
+    user = User.find_by_email(params[:email])
+    if user
+      if user.password == params[:password]
+        session[:user_id] = user.id
+        flash[:welcome] = "Welcome"
+        redirect_to users_path
+      end
+    else
+      flash[:alert] = "Invalid Credentials"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:alert] = "Logged Out"
+    redirect_to root_path
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+def new
+  @user = User.new
 end
+
+def index
+  @users = User.all
+  redirect_to new_session_path unless current_user
+end
+
+def create
+  @user = User.create user_params
+  if @user.valid?
+    flash[:alert] = "Thank you for join us"
+    redirect_to users_path
+  else
+    flash[:alert] = "Something wrong, try again please"
+    render :new
+  end
+end
+
+def destroy
+    @user = User.find params[:id]
+    @user.destroy!
+    session.clear
+    flash[:success] = "User deleted"
+    redirect_to root_path
+  end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, 
+                                 :phone, 
+                                 :fname, 
+                                 :lname, 
+                                 :password, 
+                                 :password_confirmation )
+  end
+end
+
